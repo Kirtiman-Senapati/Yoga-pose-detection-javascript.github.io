309a357247a59fb013c3b562ef4e1fc734a17d8d
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2023-01-14 17:48:08 +0530

    fun fact apply fill colour and create eclipse etc]

--- index.js
+++ index.js
@@ -1,1 +1,91 @@
-alert("hello");
+//alert("hello");
+
+
+//Main Code working related setup //
+
+/*function setup()
+{
+    createCanvas(800,500);
+}
+
+
+//Main Code working related draw //
+
+function draw()
+{
+    //1.draw images //
+    //2.play videoes //
+    //1st learn how create shapes/poins //
+    background(200);
+
+    //a. for points//
+    /*point(200,200);
+    //for points//
+
+    //b. for lines//
+    line(200,200, 300,300);
+    
+    
+    //c.for  triangle//
+    triangle(100,200,300,400,150,450);
+    //for  triangle//
+
+    
+    //d. for  rectangle//
+        rect(500,200,200,100);
+    //for  rectangle//
+
+    
+    //e. for  Circle//
+    ellipse(600,300, 100, 100);
+    //for  circle//*/
+
+    /*2nd learn how create stokes/outline and colour //
+
+    stroke(255, 0, 0,255);//for red 255,green 0,blue 0, [a=opacity]  0-255 opacity when value increase comes near 255 so stroke opicity increase near 0 increase transparent , for line thickness use  strokeWeight(weight) colour fil pain use fill method it contains 3 colour value and one opacity value //
+    ellipse(100,300, 100, 100);
+    stroke(0, 255, 0,);
+    strokeWeight(3);
+    ellipse(250,300, 100, 100);
+    stroke(0,0,255,0 );
+    ellipse(400,300, 100, 100);
+
+    ellipse(500,300, 100, 100);
+    ellipse(600,300, 100, 100);
+    
+
+}*/
+
+
+//Main Code Of programme //
+
+
+/*features and Important Notes 
+
+1. setup funtion under code run once and draw function under code runs multitimes.
+
+*/
+
+
+
+function setup()
+{
+    createCanvas(800,500);
+    console.log('Setup Fuction');
+}
+
+function getRandomArbitary(min,max)
+{
+    return Math.random() * (max-min) + min;
+}
+
+
+function draw()
+{
+    background(200);
+    //console.log('Draw function');
+    //fill(255,);
+    //ellipse(mouseX, mouseY, 50, 50);
+
+}
+
