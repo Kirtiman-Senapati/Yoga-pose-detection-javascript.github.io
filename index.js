8e51b4792ef71a046b29479326a36da10adfd0af
Kirtiman Senapati <101942178+Kirtiman-Senapati@users.noreply.github.com>
2023-01-18 23:40:02 +0530

    undo  steps

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ let singlePose ,skeleton;
 let img1;
 function setup()
 {
-    createCanvas(1200,1000);
+    createCanvas(800,500);
     capture = createCapture(VIDEO)
     capture.hide();
     posenet = ml5.poseNet(capture, modelLoaded);
